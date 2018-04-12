0db126b8165285356016072b8eac80a67eb7e8ce
Tony G <TheXardas@gmail.com>
2018-04-12 13:33:28 +0300

    Lazy-setting up counter + added redis server deploy (without connection yet)

--- src/helpers/pageViewCounter.js
+++ src/helpers/pageViewCounter.js
@@ -1,29 +1,37 @@
 import RedisMetrics from 'redis-metrics';
 
-// TODO move to config
-const metrics = new RedisMetrics({
-    host: 'redis',
-    port: '6379',
-});
-
-metrics.client.on('error', (err) => {
-    console.error(err);
-    throw err;
-});
-
-const pageViewCounter = metrics.counter('pageview', { timeGranularity: 'second' });
-
 export default {
+    counterInstance: null,
+
+    getCounter() {
+        if (!this.counterInstance) {
+            // lazy-setting up a counter instance, so we won't set up a connection, when not needed.
+            const metrics = new RedisMetrics({
+                host: 'redis',
+                port: '6379',
+            });
+
+            metrics.client.on('error', (err) => {
+                console.error(err);
+                throw err;
+            });
+
+            this.counterInstance = metrics.counter('pageview', { timeGranularity: 'second' });
+        }
+        return this.counterInstance;
+    },
+
     incr() {
         // Basically that just runs redis's ZINCRBY with a key, granulated by second
         // Only last 10 minutes are stored, so old keys will expire automatically
-        return pageViewCounter.incr();
+        return this.getCounter().incr();
     },
+
     async getLastMinuteCount(now = Date.now()) {
         const dateMinuteAgo = new Date(now - 1000 * 60);
 
         // That fetches ZSCORE's for all keys, granulated by second.
-        const response = await pageViewCounter.countRange('second', dateMinuteAgo, now);
+        const response = await this.getCounter().countRange('second', dateMinuteAgo, now);
 
         // Sum it all up.
         return Object.keys(response).reduce((sum, key) => sum + response[key], 0);
