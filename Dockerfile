d38ad0995fcca94887e6c3741ff8b376a4219645
Tony G <TheXardas@gmail.com>
2018-04-13 18:49:23 +0300

    Added aws instance id to output

--- Dockerfile
+++ Dockerfile
@@ -13,5 +13,6 @@ COPY --from=builder /usr/bin/node /usr/bin
 COPY --from=builder /usr/lib/libgcc* /usr/lib/libstdc* /usr/lib/
 COPY --from=builder /usr/src/page-view-counter-app/dist/ ./
 COPY --from=dependencies /usr/src/page-view-counter-app/node_modules ./node_modules
+CMD CONTAINER_NAME=$(curl http://169.254.169.254/latest/meta-data/instance-id --max-time 3) && export CONTAINER_NAME
 EXPOSE 8080
 CMD node ./index.js
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import ip from 'ip';
 import runServer from './helpers/runServer';
 import config from './config';
 import pageViewCounter from './helpers/pageViewCounter';
@@ -9,7 +8,7 @@ runServer(host, port, (req, res) => {
     pageViewCounter.incr();
     return pageViewCounter.getLastMinuteCount().then(count => {
         return `Hello World! Current page view count is ${count}.
-            Instance: ${ip.address()}
+            Instance: ${config.containerName}
         `;
     });
 });
