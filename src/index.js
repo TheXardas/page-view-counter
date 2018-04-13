6f62283ea1482a4f5be22ee1a9a9bcea0f897d31
Tony G <TheXardas@gmail.com>
2018-04-13 18:21:15 +0300

    Added ip output

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import ip from 'ip';
 import runServer from './helpers/runServer';
 import config from './config';
 import pageViewCounter from './helpers/pageViewCounter';
@@ -8,7 +9,7 @@ runServer(host, port, (req, res) => {
     pageViewCounter.incr();
     return pageViewCounter.getLastMinuteCount().then(count => {
         return `Hello World! Current page view count is ${count}.
-            Instance: ${config.containerName}
+            Instance: ${ip.address()}
         `;
     });
 });
