0272d63bb990dc939556219c6f3cca45e9f08fc4
Tony G <TheXardas@gmail.com>
2018-04-16 14:34:29 +0300

    Added a mock for redis counter, so tests work even without redis now.

--- test/index.js
+++ test/index.js
@@ -1,14 +1,42 @@
 import http from 'http';
 import assert from 'assert';
+import sinon from 'sinon';
+import pageViewCounter from '../src/helpers/pageViewCounter';
 import config from '../src/config';
 
 import '../src/index.js';
 
+const makeRequest = (callback) => {
+    http.get(`http://${config.host}:${config.port}`, callback);
+};
+
+// TODO write more tests
 describe('Example Node Server', () => {
+    const sandbox = sinon.sandbox.create();
+    beforeEach(() => {
+        sandbox.stub(pageViewCounter, 'incr');
+        sandbox.stub(pageViewCounter, 'getLastMinuteCount').callsFake(() =>
+            Promise.resolve(123)
+        );
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
     it('should return 200', done => {
-        http.get(`http://${config.host}:${config.port}`, res => {
+        makeRequest((res) => {
             assert.equal(200, res.statusCode);
             done();
         });
     });
+
+    it('should call incr and getLastMinutes methods', done => {
+        makeRequest((res) => {
+            //console.log(res);
+            sinon.assert.calledOnce(pageViewCounter.incr);
+            sinon.assert.calledOnce(pageViewCounter.getLastMinuteCount);
+            done();
+        });
+    })
 });
